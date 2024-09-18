ff66ba4c1734980c2c62db2616e929f0e0b78cb7
6yeey6 <leasonye730@gmail.com>
2024-09-18 17:47:34 +0800

    导出word格式

--- ruoyi-admin/src/main/java/com/ruoyi/system/service/impl/QuestionnaireServiceImpl.java
+++ ruoyi-admin/src/main/java/com/ruoyi/system/service/impl/QuestionnaireServiceImpl.java
@@ -2,6 +2,7 @@ package com.ruoyi.system.service.impl;
 
 import java.util.List;
 import com.ruoyi.common.utils.DateUtils;
+import com.ruoyi.common.utils.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.ruoyi.system.mapper.QuestionnaireMapper;
@@ -55,6 +56,43 @@ public class QuestionnaireServiceImpl implements IQuestionnaireService
     public int insertQuestionnaire(Questionnaire questionnaire)
     {
         questionnaire.setCreateTime(DateUtils.getNowDate());
+        //运营商不为空
+//        if (StringUtils.isNotEmpty(questionnaire.getOperator())){
+//        String[] splitCompanies = questionnaire.getOperator().split(",");
+//        for (String company : splitCompanies) {
+//            // 去除可能存在的空格（如果逗号后有空格的话）
+//            if ("联通".equals(company)) {
+//                questionnaire.setSecInfo2("联通");
+//            }
+//            if ("电信".equals(company)) {
+//                questionnaire.setSecInfo3("电信");
+//            }
+//            if ("移动".equals(company)) {
+//                questionnaire.setSecInfo4("移动");
+//            }
+//            if ("教育".equals(company)) {
+//                questionnaire.setSecInfo5("教育");
+//            }
+//            if ("广电".equals(company)) {
+//                questionnaire.setSecInfo6("广电");
+//            }
+//            if ("阿里云".equals(company)) {
+//                questionnaire.setSecInfo7("阿里云");
+//            }
+//            if ("腾讯云".equals(company)) {
+//                questionnaire.setSecInfo8("腾讯云");
+//            }
+//            if ("华为云".equals(company)) {
+//                questionnaire.setSecInfo9("华为云");
+//            }
+//            if ("亚马逊云".equals(company)) {
+//                questionnaire.setSecInfo10("亚马逊云");
+//            }
+//            if ("微软云".equals(company)) {
+//                questionnaire.setSecInfo11("微软云");
+//            }
+//         }
+//        }
         return questionnaireMapper.insertQuestionnaire(questionnaire);
     }
 
@@ -67,6 +105,42 @@ public class QuestionnaireServiceImpl implements IQuestionnaireService
     @Override
     public int updateQuestionnaire(Questionnaire questionnaire)
     {
+//        if (StringUtils.isNotEmpty(questionnaire.getOperator())){
+//            String[] splitCompanies = questionnaire.getOperator().split(",");
+//            for (String company : splitCompanies) {
+//                // 去除可能存在的空格（如果逗号后有空格的话）
+//                if ("联通".equals(company)) {
+//                    questionnaire.setSecInfo2("联通");
+//                }
+//                if ("电信".equals(company)) {
+//                    questionnaire.setSecInfo3("电信");
+//                }
+//                if ("移动".equals(company)) {
+//                    questionnaire.setSecInfo4("移动");
+//                }
+//                if ("教育".equals(company)) {
+//                    questionnaire.setSecInfo5("教育");
+//                }
+//                if ("广电".equals(company)) {
+//                    questionnaire.setSecInfo6("广电");
+//                }
+//                if ("阿里云".equals(company)) {
+//                    questionnaire.setSecInfo7("阿里云");
+//                }
+//                if ("腾讯云".equals(company)) {
+//                    questionnaire.setSecInfo8("腾讯云");
+//                }
+//                if ("华为云".equals(company)) {
+//                    questionnaire.setSecInfo9("华为云");
+//                }
+//                if ("亚马逊云".equals(company)) {
+//                    questionnaire.setSecInfo10("亚马逊云");
+//                }
+//                if ("微软云".equals(company)) {
+//                    questionnaire.setSecInfo11("微软云");
+//                }
+//            }
+//        }
         return questionnaireMapper.updateQuestionnaire(questionnaire);
     }
 
