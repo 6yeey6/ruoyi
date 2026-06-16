0cd2aa6d4abf91321dc1f34bd07cb13bb442c1e3
6yeey6 <leasonye730@gmail.com>
2026-06-16 14:14:45 +0800

    启动文件修改

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -10,8 +10,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @author ruoyi
  */
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-public class RuoYiApplication
-{
+public class RuoYiApplication {
     public static void main(String[] args)
     {
         // System.setProperty("spring.devtools.restart.enabled", "false");
